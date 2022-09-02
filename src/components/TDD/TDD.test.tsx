560fb7a3dd080a8022baeeefa6126b328fa67be6
saiful6356 <md.saiful.doctorkoi@gmail.com>
2022-09-02 15:31:31 +0600

    implement group tset by describe

--- src/components/TDD/TDD.test.tsx
+++ src/components/TDD/TDD.test.tsx
@@ -1,14 +1,18 @@
 import {screen, render} from '@testing-library/react';
 import { TDD } from './TDD';
 
-test.only('TDD world test', () => {
-    render(<TDD/>)
-    const testElement = screen.getByText(/TDD/i);
-    expect(testElement).toBeInTheDocument()
-})
-
-test.skip('TDD world test with name', () => {
-    render(<TDD name = 'Saiful'/>)
-    const testElement = screen.getByText(/TDD saiful/i);
-    expect(testElement).toBeInTheDocument()
+// we can use describe.only and describe.skip to test or skip specific test case  
+describe('Group test', () => {
+    test.only('TDD  test', () => {
+        render(<TDD/>)
+        const testElement = screen.getByText(/TDD/i);
+        expect(testElement).toBeInTheDocument()
+    })
+    
+    test.skip('TDD test with name', () => {
+        render(<TDD name = 'Saiful'/>)
+        const testElement = screen.getByText(/TDD saiful/i);
+        expect(testElement).toBeInTheDocument()
+    })
+    
 })
