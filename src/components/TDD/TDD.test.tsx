323373249bf4f53a851c1ffc99247b1684e1f123
saiful6356 <md.saiful.doctorkoi@gmail.com>
2022-09-02 18:14:37 +0600

    add coverage for all component can test under coverage

--- src/components/TDD/TDD.test.tsx
+++ src/components/TDD/TDD.test.tsx
@@ -1,8 +1,10 @@
+//test file name could be name.spec.tsx 
 import {screen, render} from '@testing-library/react';
 import { TDD } from './TDD';
 
 // we can use describe.only and describe.skip to test or skip specific test case  
 describe('Group test', () => {
+    // we can use 'it' instead of 'test' and 'fit' for 'test.only' and 'xit' for 'test.skip'
     test.only('TDD  test', () => {
         render(<TDD/>)
         const testElement = screen.getByText(/TDD/i);
